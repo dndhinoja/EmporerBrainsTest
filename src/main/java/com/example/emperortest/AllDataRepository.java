0143a38e57212ba3ab3782a73a96c8a579c119e3
dndhinoja <dev.dipali.dhinoja@gmail.com>
2022-07-07 05:55:20 -0700

    Monthwise BillDetails for each Home-updated 2

--- src/main/java/com/example/emperortest/AllDataRepository.java
+++ src/main/java/com/example/emperortest/AllDataRepository.java
@@ -19,35 +19,40 @@ public class AllDataRepository {
         billList.add(new Bill(2 ,1000L,Month.JAN,1));
         billList.add(new Bill(3 ,1000L,Month.JAN,2));
         billList.add(new Bill(4,2000L, Month.FEB, 1));
+        billList.add(new Bill(5,10000L, Month.JAN, 2));
+        billList.add(new Bill(6,10000L, Month.FEB, 2));
 
         Map<Home, List<BillDetails>> detailsMap = new HashMap<>();
+        
         for(int i = 0; i<homeList.size(); i++){
             Home homeTemp = homeList.get(i);
-            Long totalAmount = 0L;
             Long totalAmountJan = 0L;
             Long totalAmountFeb = 0L;
             Month month = null;
-            List<BillDetails> billDetailsList = new ArrayList<>();
+            List<BillDetails> billDetailsList = new ArrayList<>(); //BillDetails for each Home monthwise
+
             for (int j = 0; j< billList.size(); j++){
 
                 Bill billTemp = billList.get(j);
 
                 if(billTemp.homeId==homeTemp.homeId){
                     month = billTemp.getMonth();
+
+                    //Calculate Monthwise total
                     if(month.equals(Month.JAN)){
                         totalAmountJan = totalAmountJan + billTemp.getAmount();
                     }
                     else if(month.equals(Month.FEB)){
                         totalAmountFeb = totalAmountFeb + billTemp.getAmount();
                     }
-                       //totalAmount = totalAmount + billTemp.getAmount();
-                       //month = billTemp.getMonth();
                 }
             }
             BillDetails billDetailsJan = new BillDetails(Month.JAN,totalAmountJan);
             BillDetails billDetailsFeb = new BillDetails(Month.FEB,totalAmountFeb);
+
             billDetailsList.add(billDetailsJan);
             billDetailsList.add(billDetailsFeb);
+
             detailsMap.put(homeTemp, billDetailsList);
         }
         return detailsMap;
